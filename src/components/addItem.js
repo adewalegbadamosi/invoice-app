715d3e770a2b9b15b43716b68559bab910772cfd
adewalegbadamosi <adewalegbadamosi16@gmail.com>
2019-02-18 04:07:26 +0100

    added features such as currency option, flexible UIs

--- src/components/addItem.js
+++ src/components/addItem.js
@@ -4,8 +4,7 @@ import React from 'react';
 class AddItem extends React.Component {
     constructor(props){
         super(props);
-        this.handleSubmit  =  this.handleSubmit.bind(this); 
-        this.handleDelete  =  this.handleDelete.bind(this);        
+        this.handleSubmit  =  this.handleSubmit.bind(this);                 
     }
 
     handleSubmit(e){
@@ -15,27 +14,39 @@ class AddItem extends React.Component {
         this.refs.quantity.value,
         this.refs.price.value,  
         this.refs.newName.value,
-        this.refs.address.value 
+        this.refs.address.value,
+        this.refs.currency.value       
         );        
     }
-
-    handleDelete(e){
-        e.preventDefault();
-        this.props.onDelete();
-    }
+   
 
     render(){
         return(
             <div className="addTodo">
             <form  onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="item" required ref="item"/>                
-                <input type="text" placeholder="quantity" required ref="quantity"/>
-                <input type="text" placeholder="price" required ref="price"/>                
+                <input type="text" placeholder="Item" required ref="item"/>                
+                <input type="text" placeholder="Quantity" required ref="quantity"/>
+                <input type="text" placeholder="Price" required ref="price"/>                
                 <input type="text" id='company' placeholder="Company name"  ref="newName"/>  
                 <input type="text" id='address' placeholder="address/email/phone"  ref="address"/> 
+                <select name="currency" ref='currency' className="currency" >
+                  <option value="" disabled selected>Choose currency</option>
+                  <option value="NGN">Nigeria NGN</option>
+                  <option value="GHS">Ghana GHS</option>
+                  <option value="USD">US USD</option>
+                  <option value="CAD">Canada CAD</option> 
+                  <option value="EUR">Euro EUR </option>
+                  <option value="KPW">North Korea KPW</option>
+                  <option value="KRW">South Korea KRW</option>
+                  <option value="RUB"> Russia RUB</option>
+                  <option value="SAR">Saudi SAR</option>
+                  <option value="SGD">Singapore SGD</option>
+                  <option value="GBP">UK GBP</option>
+                  <option value="CNY">China CNY</option>                 
+                </select>                
                 <input type="submit" value="Create" />
             </form>
-             <button id="deleteBtn" onClick={this.handleDelete} > Delete </button>
+            
        </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,31 +4,40 @@ import ReactDOM from "react-dom";
 //Module requires
 import AddItem from './components/addItem';
 import TodoItem from './components/todoItem';
+import Balance from './components/Balance';
 
 //Create a component
 class TodoComponent extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
-            
+        this.state = {             
                     todos: [],
                     sum:'',
                     count:'',
                     company: 'Your company Name',
-                    address: 'Your address'             
+                    address: 'Your address',
+                    currency:'',
+                    cash: '',
+                    discount: 0,
+                    vat:0 ,
+                    balance:''            
                 };
         this.onDelete = this.onDelete.bind(this);
         this.onAdd = this.onAdd.bind(this); 
-              
+        this.cashAnalysis = this.cashAnalysis.bind(this);  
+        this.showInputField = this.showInputField.bind(this);            
         }
         
     //Custom functions
-       onAdd(item,  quantity, price, company,address) {
+  
+    
+       onAdd(item,  quantity, price, company,address, currency) {
         var updatedTodos = this.state.todos;
         var updatedSum = this.state.sum;
         var updatedCount = this.state.count; 
         var updatedCompany = this.state.company; 
         var updatedAddress = this.state.address; 
+        var updatedCurrency = this.state.currency;
 
         var filter = updatedTodos.filter(function(val, index){            
             return  val.item == item  ;                             
@@ -38,8 +47,8 @@ class TodoComponent extends React.Component {
             var firstContent = {
                 item : item,
                 quantity : Number(quantity),
-                price : Number(price),
-                amount : Number(price * quantity)
+                price : Number((price*1).toFixed(2)) ,
+                amount : Number((price * quantity).toFixed(2))
             }
             updatedTodos.push(firstContent);
             updatedSum = firstContent.amount ;
@@ -47,18 +56,20 @@ class TodoComponent extends React.Component {
                 var anotherContent = {
                     item : item,
                     quantity : Number(quantity),
-                    price : Number(price),
-                    amount : Number(price * quantity)
+                    price : Number((price*1).toFixed(2)) ,
+                    amount : Number((price * quantity).toFixed(2))
                 }
                 updatedTodos.push(anotherContent);
-                updatedSum = Number(updatedSum) + anotherContent.amount ; 
-               }
+                updatedSum += Number( anotherContent.amount.toFixed(2)) ; 
+               
+            }
             else{
                 for(var i = 0; i< updatedTodos.length; i++) {
                 if(updatedTodos[i].item == item){                                        
                 updatedTodos[i].quantity += Number(quantity);                       
-                updatedTodos[i].amount +=  Number(price * quantity) ;                
-                updatedSum +=  Number(price * quantity) ;                
+                updatedTodos[i].amount +=  Number((price * quantity).toFixed(2)) ;                            
+                updatedSum = updatedSum + Number(price*quantity)  ; 
+                            
                     }
                }
             }
@@ -66,14 +77,37 @@ class TodoComponent extends React.Component {
 
             updatedCount = Number(updatedCount) + Number(quantity) ;
             updatedCompany =  !company  ?  updatedCompany : company ; 
-            updatedAddress =  !address  ?  updatedAddress : address ; 
+            updatedAddress =  !address  ?  updatedAddress : address ;
+            updatedCurrency =  !currency  ?  updatedCurrency : currency ;            
       
         this.setState({
             todos: updatedTodos,
             sum: updatedSum,
             count: updatedCount,
             company: updatedCompany,
-            address: updatedAddress
+            address: updatedAddress,
+            currency: updatedCurrency
+        });
+    }
+
+    cashAnalysis(cash,discount,vat) {        
+        var updatedSum = this.state.sum ;       
+        var updatedCash = this.state.cash;
+        var updatedDiscount = this.state.discount;
+        var updatedVat = this.state.vat;         
+        var updatedBalance = this.state.balance; 
+            
+        updatedCash =  !cash  ?  updatedCash : (cash *1).toFixed(2);
+        updatedDiscount =  !discount  ?  updatedDiscount : (discount /100 * updatedSum).toFixed(2) ;
+        updatedVat =  !vat  ?  updatedVat : (vat/100 * updatedSum).toFixed(2) ; 
+        updatedBalance = Number(updatedCash) + Number(updatedDiscount) - Number(updatedVat) - Number(updatedSum);
+        updatedBalance = (updatedBalance *1).toFixed(2);
+
+        this.setState({
+            cash: updatedCash,
+            discount: updatedDiscount,
+            vat: updatedVat,
+            balance: updatedBalance
         });
     }
 
@@ -82,6 +116,10 @@ class TodoComponent extends React.Component {
         var indexToDelete= updatedTodos.indexOf(item);        
         var updatedSum = this.state.sum; 
         var updatedCount = this.state.count; 
+        var updatedCash = this.state.cash;
+        var updatedDiscount = this.state.discount;
+        var updatedVat = this.state.vat; 
+        var updatedBalance = this.state.balance;
 
        if(item){
           updatedTodos = updatedTodos.filter(function(val, index){            
@@ -92,60 +130,85 @@ class TodoComponent extends React.Component {
        } else{
         updatedTodos = [];
         updatedSum = '';     
-        updatedCount =''; 
-       }
-       
+        updatedCount ='';
+        updatedCash ='';
+        updatedDiscount = 0;
+        updatedVat = 0; 
+        updatedBalance ='';  
+       }       
     
     this.setState({
       todos: updatedTodos,
       sum: updatedSum,
-      count:  updatedCount
+      count:  updatedCount,
+      cash: updatedCash ,
+      discount: updatedDiscount ,
+      vat:  updatedVat,        
+      balance:  updatedBalance
     });
 }  
-      
+showInputField(e){
+    e.preventDefault();   
+    this.setState({
+        todos: [],
+        sum: '',
+        count:  '',
+        cash: '' ,
+        discount: 0 ,
+        vat:  0,        
+        balance:  ''
+    })  
+}    
     
     render() {       
-        var todos = this.state.todos;         
-
+        var todos = this.state.todos; 
         todos = todos.map(function(content, index){
             return( 
-             <tr>                    
+              <tr>                    
                         <td className="index"  > {index+1}  </td>
                         <td className="itemName" > {content.item}  </td>
                         <td className="itemQuantity" > {content.quantity} </td>
                         <td className="itemPrice"  > {content.price} </td>                        
-                        <td> <TodoItem  content={content} onDelete={this.onDelete} />  </td>                                                                 
+                        <td className="tdTodoItem" > <TodoItem  content={content} onDelete={this.onDelete} />  </td>                                                                 
               </tr>   
                  );
-           }.bind(this));            
-           
+           }.bind(this));  
 
         return (
             <div className="todoList">                
                 
                 <h1> {this.state.company}</h1>
-                <h3> {this.state.address}</h3>
-                
-                <h2>Sales Invoice</h2>
-                
+                <h3> {this.state.address}</h3>                
+                <h2>Sales Invoice</h2>                
                 <table > 
                         <tr >
                             <th> S/N </th>
                             <th> Item </th>
                             <th> Qty </th>
                             <th> Price </th>
-                            <th> Amount </th>
+                            <th> Amount ({this.state.currency}) </th>
                         </tr>
                         {todos}                    
                 </table>                
-                <div className="h5">
-                    <h5> Total ({this.state.count})  N{this.state.sum} </h5>                
-                                    
-                </div>
-                <h6> Thanks for Patronage </h6>
-                <p> { Date()} </p>
-                <h4><AddItem onAdd={this.onAdd} onDelete={this.onDelete} /> </h4>
                 
+                <h5> Invoice total ({this.state.count})  {this.state.sum} </h5> 
+                <div className="cashAnalysis">
+                    <h6> Cash paid   {this.state.cash} </h6> 
+                    <h6> Discount(%) {this.state.discount} </h6>  
+                    <h6> VAT(%) ({this.state.vat}) </h6>
+                    <h6> Invoice total ({this.state.sum})  </h6>
+                </div>
+                <h5 id='invoiceBalance'> Invoice balance   {this.state.balance} </h5> 
+
+                <p style={this.state.balance ? {display:'block'} : {display:'none'}} > Thanks for Patronage </p>
+                <p style={this.state.balance ? {display:'block'} : {display:'none'}} >  { Date()} </p>
+
+            <div className='inputField'>    
+                <h4 style={!this.state.balance ? {display:'block'} : {display:'none'}}><AddItem onAdd={this.onAdd}  /> </h4>
+                <h4 style={!this.state.balance ? {display:'block'} : {display:'none'}} ><Balance cashAnalysis={this.cashAnalysis} onDelete={this.onDelete} /> </h4>
+            </div>    
+                <button id='create' style={this.state.balance ? {display:'block'} : {display:'none'}} onClick={this.showInputField} > Create New  </button>
+            
             </div>
         );
     }
